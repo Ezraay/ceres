017e3284f50ba06f51099589d1a66f7523c21aaa
Valeriy <valeriy.djulai@gmail.com>
2022-12-25 21:24:39 +1100

    refactor: Combat Manager tests

--- Assets/Scripts/Battle/CombatManager.cs
+++ Assets/Scripts/Battle/CombatManager.cs
@@ -6,8 +6,8 @@ namespace CardGame
     {
         public bool ValidAttack => Attacker != null;
         public readonly MultiCardSlot Defenders = new MultiCardSlot();
-        private CardSlot target;
-        public CardSlot Attacker;
+        public CardSlot Target { get; private set; }
+        public CardSlot Attacker { get; private set; }
         private readonly int damage = 1;
 
         public void AddAttacker(CardSlot slot)
@@ -18,17 +18,19 @@ namespace CardGame
 
         public void AddTarget(CardSlot slot)
         {
-            target = slot;
+            Target = slot;
         }
 
         public void AddDefender(ICard card)
         {
-            Defenders.AddCard(card);
+            if (card != null)
+                Defenders.AddCard(card);
         }
 
         public void Reset(MultiCardSlot graveyard)
         {
             Attacker = null;
+            Target = null;
             foreach (var defender in Defenders.Cards) graveyard.AddCard(defender);
             Defenders.Clear();
         }
@@ -37,7 +39,7 @@ namespace CardGame
         {
             int attack = Attacker.Card.Data.Attack;
             int defense = Defenders.Cards.Sum(card => card.Data.Defense);
-            return defense + target.Card.Data.Attack > attack ? 0 : damage;
+            return defense + Target.Card.Data.Attack > attack ? 0 : damage;
         }
     }
 }
