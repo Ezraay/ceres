ea5e91100238e7a496217adbd54803de1c22ebb5
Dmitri <djuly@yahoo.com>
2022-12-29 23:23:23 +1100

    Started on building list of users in the Lobby

---
+++ Server/Entities/HubGameClient.cs
@@ -0,0 +1,9 @@
+namespace Entities
+{
+    public class HubGameClient
+    {
+        public string? ConnectionId { get; set; }
+        // Can have whatever you want here
+    }
+    
+}
--- Server/Hubs/LobbyHub.cs
+++ Server/Hubs/LobbyHub.cs
@@ -1,15 +1,26 @@
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
+using Entities;
 
 
 
 public class LobbyHub : Hub
 {
     public static int ClientsConnected { get; set;}
+    public static ConcurrentDictionary<string, HubGameClient> MyUsers = new ConcurrentDictionary<string, HubGameClient>();
 
     public override Task OnConnectedAsync()
     {
         ClientsConnected++;
         Clients.All.SendAsync("ClientsOnServer",ClientsConnected).GetAwaiter().GetResult();
+        
+        var connectionId = Context.ConnectionId;
+        Console.WriteLine($"User connected with ID: {connectionId}");
+        // var userId = Context?.User?.Identity?.Name; // any desired user id
+        lock(MyUsers){
+            MyUsers.TryAdd(connectionId, new HubGameClient() { ConnectionId = connectionId });
+        }
+        
         return base.OnConnectedAsync();
     }
 
@@ -17,6 +28,13 @@ public class LobbyHub : Hub
     {
         ClientsConnected--;
         Clients.All.SendAsync("ClientsOnServer",ClientsConnected).GetAwaiter().GetResult();
+
+        Console.WriteLine($"User disconnected with ID: {Context.ConnectionId}");
+        lock(MyUsers){
+            HubGameClient? garbage;
+            MyUsers.TryRemove(Context.ConnectionId, out garbage);
+        }
+        
         return base.OnDisconnectedAsync(exception);
     }
 
