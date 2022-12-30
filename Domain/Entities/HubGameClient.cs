520becf9c2f518a9c904046900b07c7d38c35451
Dmitri <djuly@yahoo.com>
2022-12-30 23:51:19 +1100

    Added Domain class lib

--- Domain/Entities/HubGameClient.cs
+++ Domain/Entities/HubGameClient.cs
@@ -1,8 +1,9 @@
-namespace Entities
+namespace Domain.Entities
 {
     public class HubGameClient
     {
         public string? UserName { get; set; }
+        public bool ReadyToPlay { get; set; } = false;
         // Can have whatever you want here
     }
     
--- Server/Hubs/LobbyHub.cs
+++ Server/Hubs/LobbyHub.cs
@@ -1,6 +1,6 @@
 using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
-using Entities;
+using Domain.Entities;
 
 
 
