e299061067fb36796486d5a9706a08159bcfc8c1
Valeriy <valeriy.djulai@gmail.com>
2023-01-07 19:50:53 +1100

    fix: CSV delimiter fix

--- Assets/Scripts/Core/BattleSystem/Cards/CSVCardDatabase.cs
+++ Assets/Scripts/Core/BattleSystem/Cards/CSVCardDatabase.cs
@@ -12,8 +12,7 @@ namespace Ceres.Core.BattleSystem
             string[] lines = csvData.Split('\n');
             for (int i = skipHeader ? 1 : 0; i < lines.Length; i++)
             {
-                string[] values = lines[i].Split(',');
-                Debug.Log(lines[i]);
+                string[] values = lines[i].Split('|');
                 ICardData cardData = new CardData(
                     values[0],
                     values[1],
