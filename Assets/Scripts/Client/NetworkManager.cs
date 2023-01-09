96db95ec0b563603579d274efda2675cc4040f6a
Valeriy <valeriy.djulai@gmail.com>
2023-01-10 08:28:14 +1100

    feature: Main Menu and Loading screens

---
+++ Assets/Scripts/Client/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ceres.Client
+{
+    [CreateAssetMenu(menuName = "Create NetworkManager", fileName = "NetworkManager", order = 0)]
+    public class NetworkManager : ScriptableObject
+    {
+        public event Action OnConnected; // TODO: Call this when connected
+        public event Action OnJoinQueue;
+        
+        public void Connect()
+        {
+            // TODO: Connect
+            // var client = new NetworkClient() i dunno smth like this
+        }
+
+        public void JoinQueue()
+        {
+            // TODO: Join queue
+        }
+    }
+}
