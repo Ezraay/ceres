5157faa8b019f07df88320e48daeabeb84107ae7
Dmitri <djuly@yahoo.com>
2023-02-05 23:24:02 +1100

    feature: Server - started on player actions

--- Server/wwwroot/js/game.js
+++ Server/wwwroot/js/game.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var GameHubConnection = new signalR.HubConnectionBuilder().withUrl("/GameHub").build();
+var Player1Turn = true;
 
 
 
@@ -16,6 +17,13 @@ function fulfilled() {
     
     GameHubConnection.invoke("JoinGame", gameId, userId).then((res) => {
         console.log(res)
+        if (res == "JoinedAsPlayer1") {
+            document.getElementById("P2Action").hidden = true;
+            document.getElementById("P1Action").disabled = !Player1Turn;
+        }
+        if (res == "JoinedAsPlayer2") {
+            document.getElementById("P1Action").hidden = true;
+        }
         
     });
     
@@ -33,12 +41,22 @@ GameHubConnection.on("UpdatePlayersName", (p1Name, p2Name) => {
     // document.getElementById("P1Action").disabled = false;
 })
 
-// GameHubConnection.on("Player2Joined", (p2Name) => {
-//     console.log("Player2Joined")
-//     document.getElementById("P2Action").disabled = false;
-// })
 
 GameHubConnection.start().then(fulfilled, rejected)
 .catch(function (err) {
     return console.error(err.toString());
 });
+
+
+
+
+document.getElementById("P1Action").addEventListener("click", PlayerActon);
+document.getElementById("P2Action").addEventListener("click", PlayerActon);
+
+function PlayerActon(){
+    console.log("playerAction");
+
+    // document.getElementById("P1Action").disabled = !Player1Turn;
+    // document.getElementById("P2Action").disabled = Player1Turn;
+
+}
