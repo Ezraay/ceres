804a7793fba9e6a50fde469504768728cfac8278
Dmitri <djuly@yahoo.com>
2023-02-22 00:00:27 +1100

    feature: js client cards display

--- Server/wwwroot/js/game.js
+++ Server/wwwroot/js/game.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 var GameHubConnection = new signalR.HubConnectionBuilder()
     // .withHubProtocol(new signalR.JsonHubProtocol({
     //     transferFormat: signalR.TransferFormat.Text,
@@ -10,6 +11,9 @@ var Player1Turn = true;
 
 let userId = sessionStorage.getItem("userId");
 let gameId = sessionStorage.getItem("gameId");
+
+var ImPlayer1 = false; 
+var ImPlayer2 = false; 
 
 
 function fulfilled() {
@@ -23,13 +27,16 @@ function fulfilled() {
     GameHubConnection.invoke("JoinGame", gameId, userId).then((res) => {
         console.log(res)
         if (res == "JoinedAsPlayer1") {
+            ImPlayer1 = true;
             document.getElementById("P2Action").hidden = false;
             document.getElementById("P1Action").disabled =false;
-            // document.getElementById("P1Action").disabled = !Player1Turn;
+            document.getElementById("player1Name").innerText += " (You)"
         }
         if (res == "JoinedAsPlayer2") {
+            ImPlayer2 = true;
             document.getElementById("P1Action").hidden = false;
             document.getElementById("P2Action").disabled =false;
+            document.getElementById("player2Name").innerText += " (You)"
         }
         
     });
@@ -37,7 +44,7 @@ function fulfilled() {
 }
 
 function rejected() {
-
+    
 }
 
 
@@ -45,12 +52,52 @@ GameHubConnection.on("UpdatePlayersName", (p1Name, p2Name) => {
     console.log("UpdatePlayersName")
     document.getElementById("player1Name").innerText = p1Name;
     document.getElementById("player2Name").innerText = p2Name;
-    // document.getElementById("P1Action").disabled = false;
+    if (ImPlayer1){
+        document.getElementById("player1Name").innerText += " (You)"
+    }
+    if (ImPlayer2){
+        document.getElementById("player2Name").innerText += " (You)"
+    }
 })
 
 GameHubConnection.on("ServerAction", action => {
-    console.log("ServerAction");
     console.log(action);
+     
+    let actionObj = Object.assign(Object.prototype, action);
+
+    console.log(actionObj.card.data.name);
+
+
+
+    var li = document.createElement("li");
+    li.classList.add("nav-item");
+    var aTab = document.createElement("a");
+    aTab.classList.add("nav-link","active");
+    aTab.innerText = actionObj.card.data.name;
+    aTab.href = "#"
+    li.appendChild(aTab);
+    if (ImPlayer1){
+        var cardsul = document.getElementById("P1Cards")
+    }
+    if (ImPlayer2){
+        var cardsul = document.getElementById("P2Cards")
+    }
+
+    // removing previously active aTabs
+    var previousActiveTab = cardsul.querySelector(".nav-link.active");
+    if(previousActiveTab) {
+        previousActiveTab.classList.remove("active");
+    }
+
+    cardsul.appendChild(li);
+
+
+
+    // // append the tab pane to the tab content div
+    // var tabContentDiv = document.createElement("div");
+    // tabContentDiv.classList.add("tab-content");
+    // tabContentDiv.appendChild(tabPane);
+
 });
 
 
