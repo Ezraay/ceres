732521fbf0d390a9f7a814c2ca89862e3dd1e780
DD <djuly.dmitri@gmail.com>
2023-04-16 21:37:11 +1000

    feature: web-client SignalR Hub as service finished

---
+++ web-client/Services/ISignalRHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace webclient.Services;
+
+public interface ISignalRHub
+{
+    HubConnection LobbyHubConnection { get; }
+    Task<bool> ConnectWithRetryAsync(CancellationToken token);
+    event Action? OnHubStataHasChanged;
+}
--- web-client/Services/LobbyHub.cs
+++ web-client/Services/LobbyHub.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.SignalR.Client;
 
 namespace webclient.Services;
 
-public class LobbyHub
+
+public class LobbyHub : ISignalRHub
 {
+    public HubConnection LobbyHubConnection { get; }
+    public event Action? OnHubStataHasChanged;
     public LobbyHub(IConfiguration configuration)
     {
         var uri = $"{configuration.GetSection("ceres")["server-address"]}/{configuration.GetSection("ceres")["lobby-hub"]}";
@@ -12,8 +15,57 @@ public class LobbyHub
             .WithUrl(uri)
             .WithAutomaticReconnect()
             .Build();
+        LobbyHubConnection.Closed += NotifyUserOfServerDisconnected;
+        LobbyHubConnection.Reconnecting += NotifyUserOfReconnecting;
+        LobbyHubConnection.Reconnected += NotifyUserOfReconnected;
     }
 
-    public HubConnection LobbyHubConnection { get; }
+    public async Task<bool> ConnectWithRetryAsync( CancellationToken token){
+        // Keep trying to until we can start or the token is canceled.
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine($"Lobby Connection - trying to connect..");
+                if (LobbyHubConnection != null)
+                    await LobbyHubConnection.StartAsync(token);
+                Console.WriteLine($"Lobby Connection - connected.");
+                OnHubStataHasChanged?.Invoke();
+                return true;
+            }
+            catch when (token.IsCancellationRequested)
+            {
+                return false;
+            }
+            catch
+            {
+                // Failed to connect, trying again in 5000 ms.
+                await Task.Delay(5000, token);
+            } 
+        }
+    }
 
+    private Task NotifyUserOfReconnected(string? connectionId)
+    {
+        Console.WriteLine($"Lobby Connection successfully reconnected. The ConnectionId is now: {connectionId}");
+
+        OnHubStataHasChanged?.Invoke();
+        return Task.CompletedTask;
+    }
+
+    private async Task NotifyUserOfServerDisconnected(Exception? e)
+    {
+        Console.WriteLine(e != null ? $"Lobby Connection: Connection to server closed. Error: {e.Message}" : "Lobby Connection closed.");
+        OnHubStataHasChanged?.Invoke();
+        await ConnectWithRetryAsync(CancellationToken.None);
+    }
+    
+    private Task NotifyUserOfReconnecting(Exception? e)
+    {
+        Console.WriteLine($"Lobby Connection started to reconnect due to an error: {e!.Message}");
+        OnHubStataHasChanged?.Invoke();
+        return Task.CompletedTask;
+    }
+
+    
 }
